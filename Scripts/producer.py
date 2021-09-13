c0fd0518e82d2a6dc6dba21ce9db5c6fff9c7078
Pooja Kumari <66157184+Pooja0210@users.noreply.github.com>
2021-09-13 21:27:38 +0530

    Update producer.py

--- Scripts/producer.py
+++ Scripts/producer.py
@@ -8,11 +8,11 @@ def on_message(ws, message):
      producer.send('sample', message)
      producer.flush()
 
-# Creating Kafka Producer
+# Created Kafka Producer
 producer = KafkaProducer(value_serializer=lambda m: str(m).encode("utf-8"),
                          bootstrap_servers=['localhost:9092'])
 
-# Creating Streaming Socket
+# Created Streaming Socket
 ws = websocket.WebSocketApp("wss://stream.meetup.com/2/rsvps", on_message=on_message)
 
 # Sending the data for every 2 second
